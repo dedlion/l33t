e28bdd3eede3f780d86faa8a305945a2635b97d0
DevlioN <dedlion@gmail.com>
2023-05-21 23:35:08 +0300

    problem #10 WIP. time limit exceeded. need to optimize

--- 6_10/main.cpp
+++ 6_10/main.cpp
@@ -52,13 +52,134 @@ public:
     //Problem #9 EASY
     bool isPalindrome(int x) {
         string str = to_string(x);
-        bool even = str.size()%2==1;
         int mid = str.size()/2;    //can swap for floor(str.size()/2.0)
         for (int i=0; i<mid;i++)
         {
-            if (str.at(i)!=str.at(str.size()-i-1)) return false;
+            if (str.at(i)!=str.at(str.size()-i-1)) return false;    //compare letters. return if not
         }
         return true;
+    }
+
+    //https://leetcode.com/problems/regular-expression-matching/
+    //Problem #10 HARD
+    const char EMPTY_WILD = '\0';
+    struct partExpr{
+        string pattern;
+        char wild;
+
+        int any;
+    };
+
+    bool l_match(string input , partExpr * expr, int count)
+    {
+        partExpr currExpr;
+        if (count==0)   //no more commands
+        {
+            if (input.size()==0) //no more data
+                return true;
+            else
+                return false;
+        }
+        else
+        {
+            currExpr = *expr;   //make local copy of command
+            count--;            //counter of commands
+            if (count>0)
+                expr++;
+        }
+
+
+        if (currExpr.any!=0)    //!.
+        {
+            if (currExpr.any==-1)   //wild .
+            {
+                currExpr.any=0;
+                while(currExpr.any <= input.size())
+                {
+                    string copyInput = input;   //make cpy bcs arese modified base string. Can erase 1 letter without copy
+                    bool ok = l_match(copyInput.erase(0,currExpr.any),expr,count);
+                    if (ok) return ok;
+                    currExpr.any++; //wild . + . + . and so on
+                }
+                return false;
+            } else
+            {
+                if (currExpr.any > input.size()) return false;  //check that ... less then letters
+                return l_match(input.erase(0,currExpr.any),expr,count); //remove letters and recursive call
+            }
+        } else
+        {
+            if (currExpr.wild==EMPTY_WILD)  //is any wild letters
+            {
+                size_t found = input.find(currExpr.pattern);
+                if (found!=0)   //starts with pattern
+                    return false;
+                else
+                    return l_match(input.erase(0,currExpr.pattern.size()),expr,count);  //remove letters and recursive call
+            } else
+            {
+                //wild letters currExpr.pattern + currExpr.wild + currExpr.wild and so on
+                size_t found =0;
+                while (found!=string::npos)
+                {
+                    found = input.find(currExpr.pattern);
+                    if (found!=0) //start with pattern
+                        return false;
+                    else
+                    {
+                        string copyInput = input;
+                        bool ok = l_match(copyInput.erase(0,currExpr.pattern.size()),expr,count);
+                        if (ok) return true;
+                    }
+                    currExpr.pattern+=currExpr.wild;
+                }
+                return false;
+            }
+        }
+    }
+
+    bool isMatch(string s, string p) {
+        vector<partExpr> patterns;
+        partExpr basic_expr = {"",EMPTY_WILD,0};    //basic empty pattern
+        partExpr expr = basic_expr;
+        //Im so hate strings in c++. Qt will do it in couple of strings
+        //doing here command stack
+        for (auto i : p)
+        {
+            switch (i) {
+            case '.':
+                if (expr.pattern!="")   //prev character was !.
+                {
+                    patterns.push_back(expr);
+                    expr = basic_expr;
+                }
+                expr.any++; //optimizing ...
+                break;
+            case '*':
+                if (expr.pattern!="")
+                {
+                    expr.wild = expr.pattern.back();
+                    expr.pattern.pop_back();
+                } else
+                {
+                    expr.any=-1;
+                }
+                patterns.push_back(expr);
+                expr = basic_expr;
+                break;
+            default:    //any other character
+                if (expr.any!=0) //prev character was .
+                {
+                    patterns.push_back(expr);
+                    expr = basic_expr;
+                }
+                expr.pattern+=i;
+                break;
+            }
+        }
+        if (expr.any>0 || expr.pattern !="")
+            patterns.push_back(expr);   //add last command
+        return l_match(s,patterns.data(),patterns.size());  //this is recursive checker
     }
 };
 
@@ -74,6 +195,9 @@ int main()
 
     //Problem #9
     cout << " pal " << sol.isPalindrome(12321) << endl;
+
+    //Problem #10
+    cout << "isMatch" << sol.isMatch("a",".*..a*") << endl;
 
     return 0;
 }
