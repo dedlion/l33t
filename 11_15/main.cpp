078be4a1bdb68f9287ff2ab79da33ffd088747a8
DevlioN <dedlion@gmail.com>
2023-05-23 21:38:02 +0300

    added #11

--- 11_15/main.cpp
+++ 11_15/main.cpp
@@ -7,6 +7,32 @@ using namespace std;
 
 class Solution {
 public:
+    //https://leetcode.com/problems/container-with-most-water/
+    //Problem #11 MEDIUM
+    int maxArea(vector<int>& height) {
+        if (height.size()==0) return 0;
+        auto left = height.begin(); //leftmost iterator
+        auto right = height.end();  //rightmost iterator
+        right--;    //make it valid
+        int maxPoolSize = min((*left) , (*right)) *(right-left);    //startingValue in case vec.size()==1
+        //idea here move iterator from the sides to center
+        while (left!=right) //while there is no more combination
+        {
+            if (*left<*right)   //we move iterator with smaller value. If they == doesnt mater which one
+            {
+                left++;
+            } else
+            {
+                right--;
+            }
+            //we can calculate only if newer value is big enough.
+            //but math is simple - IMO faster to calc then check is there need to calc
+            int poolSize = min((*left) , (*right)) *(right-left);
+            maxPoolSize = max( poolSize , maxPoolSize);
+        }
+        return maxPoolSize;
+    }
+
     //https://leetcode.com/problems/longest-common-prefix/
     //Problem #14 EASY
     string longestCommonPrefix(vector<string>& strs) {
@@ -36,6 +62,9 @@ public:
 int main()
 {
     Solution sol;
+    //11#
+    vector<int> vec_11_1 = {1,8,6,2,5,4,8,3,7};
+    cout << "maxArea" << sol.maxArea(vec_11_1) << endl;
 
     //14#
     vector<string> vec_14_1 = {"flower","flow","flight"};
