b244fc8635dce4c14710060810d158c3e30fcf0c
DevlioN <dedlion@gmail.com>
2023-05-24 22:40:52 +0300

    added #15. Most intresting problem so far

--- 11_15/main.cpp
+++ 11_15/main.cpp
@@ -1,8 +1,22 @@
 #include <iostream>
 #include <string>
 #include <vector>
+#include <map>
 
 using namespace std;
+
+template <class T>
+void showVector(const vector<T> vec)
+{
+    for (auto i : vec) cout << i << "  ";
+    cout<< endl;
+}
+template <class T>
+void showVectors(const vector<vector<T>> vec)
+{
+    for (auto i : vec) showVector(i);
+    cout<< endl;
+}
 
 
 class Solution {
@@ -32,6 +46,83 @@ public:
         }
         return maxPoolSize;
     }
+
+
+    vector<vector<int>> threeSum(vector<int>& nums) {
+        vector<vector<int>> result;
+        map<int,int> positive;
+        map<int, int> negative;
+        int zeroes=0;
+        //create tw0 separate sets for + and - values
+        for (auto i : nums)
+        {
+            if (i>=0)
+            {
+                positive[i]++;
+                if (i==0)
+                {
+                    zeroes++;
+                    negative[i]++;
+                }
+            }
+            else
+                negative[i]++;
+            }
+
+           if (zeroes==2) positive[0]--; //{0,0,X} != 0
+
+
+           for (auto i = positive.begin(); i!=positive.end(); i++)
+           {
+               for (auto l = negative.begin(); l!=negative.end(); l++)
+               {
+                   int value = -(i->first + l->first); //we need to find this number A+B+C=0 -> A+B=-C
+                   if (value>=0)
+                   {
+                       //search in positive
+                       if (i->first == value)   //SAME l->first == 2 * i->first ;
+                       {
+                           if (i->second>1)
+                           {
+                               //viable {+i, +i, -2*i}
+                               result.emplace_back(std::initializer_list<int>{i->first,i->first,l->first});
+                           }
+                       } else
+                       {
+                           if (value < i->first ) continue;
+                           auto search = positive.find(value);
+                           if (search != positive.end())
+                           {
+                               //viable {+i, -l, +search}
+                               result.emplace_back(std::initializer_list<int>{i->first,l->first,search->first});
+                           }
+                       }
+                   } else
+                   {
+                       //search in negative
+                       if (l->first == value)   //SAME i->first == 2 * l->first ;
+                       {
+                           if (l->second>1)
+                           {
+                               //viable {+i, -i/2, -i/2}
+                               result.emplace_back(std::initializer_list<int>{i->first,l->first,l->first});
+                           }
+                       } else
+                       {
+                           if (value < l->first ) continue;
+                           auto search = negative.find(value);
+                           if (search != negative.end())
+                           {
+                               //viable {+i, -l, -search}
+                               result.emplace_back(std::initializer_list<int>{i->first,l->first,search->first});
+                           }
+                       }
+                   }
+               }
+           }
+           return result;
+       }
+
 
     //https://leetcode.com/problems/longest-common-prefix/
     //Problem #14 EASY
@@ -70,6 +161,9 @@ int main()
     vector<string> vec_14_1 = {"flower","flow","flight"};
     cout << " res14 " <<  sol.longestCommonPrefix(vec_14_1) << endl;
 
+    //15#
+    vector<int> vec_15_1 = {1,-1,0,0,-2,-2,4,1,-3,2};
+    showVectors(sol.threeSum(vec_15_1));
     return 0;
 }
 
