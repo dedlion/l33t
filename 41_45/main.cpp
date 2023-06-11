0a74bb16b3694d247abbd19ee7fc2c78a0d7e48e
DevlioN <dedlion@gmail.com>
2023-06-11 22:42:47 +0300

    added problem #35

--- 41_45/main.cpp
+++ 41_45/main.cpp
@@ -23,6 +23,38 @@ int firstMissingPositive(vector<int>& nums) {
     }
     return distance( begin(map), find_if( begin(map), end(map), [](int x) { return x == false; })); //first zero is missed int
 }
+
+
+int jump(vector<int>& nums) {
+    int len = nums.size();  //total vector size
+    if (len == 1) return 0; //unique solution
+    int startPoint = 0;     //starting point to leap
+    int leapsNum = 0;       //total count of leaps
+    int leapLen = nums[0];  //len of current cealp
+
+    //we find best leap. Condition -> next leap is MAX
+    int maxLeapLen = -1;    //starting impossible value
+    int maxLeanIndex = -1;
+    while (startPoint + leapLen + 1 < len)
+    {
+        for (int i=0; i<leapLen+1; i++) //check next leap max len
+        {
+            if (nums[i+startPoint]+i > maxLeapLen)  //max len = leps where we leap + len of leap
+            {
+                maxLeapLen=nums[i+startPoint]+i;
+                maxLeanIndex =i + startPoint;
+            }
+        }
+
+        leapsNum++; //+1 leap
+        leapLen = nums[maxLeanIndex];   //next leap value
+        startPoint = maxLeanIndex;      //next leap starting point
+        maxLeapLen = -1;                //clear tmp value
+        maxLeanIndex = -1;              //clear tmp value
+    }
+
+    return leapsNum+1;
+}
 };
 
 int main()
@@ -30,6 +62,9 @@ int main()
     Solution sol;
     vector<int> data41 {7,8,9,11,12};
     cout  << "missed int " << sol.firstMissingPositive(data41) << endl;
+
+    vector<int> data45 {3,6,7,1,4,2,2,2,3,5,7,6,5,1,1,1,1,3};
+    cout << "max jumps " << sol.jump(data45) << endl;
 
     return 0;
 }
