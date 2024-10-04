cc06281c01122a7a4e52207882615ae85cf25af2
Your Name <you@example.com>
2024-10-04 19:08:12 +0300

    added proble 59

--- 56_60/main.cpp
+++ 56_60/main.cpp
@@ -82,6 +82,45 @@ int lengthOfLastWord(string s) {
     return wordStart-i;
 }
 
+//PROBLEM 59 SPIRAL MATRIX II
+//internal function
+int fillOuterFields(std::vector<std::vector<int>> & matrix, int row, int startVal)
+{
+    int n = matrix.size();
+    //right up direction
+    for (int i=row; i<n-row; i++)
+        matrix[row][i]=startVal++;
+    //down right
+    for (int i=row+1; i<n-row; i++)
+        matrix[i][n-row-1]=startVal++;
+    //left down
+    for (int i=n-row-2; i>=row; i--)
+        matrix[n-row-1][i]=startVal++;
+    //up left
+    for (int i=n-row-2; i>row; i--)
+        matrix[i][row]=startVal++;
+    return startVal;
+}
+//main function
+vector<vector<int>> generateMatrix(int n) {
+    int row =0;
+    int mid = (n%2==0)?n/2:(n/2+1);
+    int startValue = 1;
+    //make empty space
+    vector<vector<int>> result;
+    result.resize(n);
+    for (int i=0; i<n; i++)
+        result[i].resize(n);
+    //fill it by continiously filled outer fields
+    //can write wihtout func call... but this is more readable IMO
+    while (row < mid)
+    {
+        startValue = fillOuterFields(result, row, startValue);
+        row++;  //now we "pretend" that our matrix is smaller
+    }
+    return result;
+}
+
 int main()
 {
     std::vector<std::vector<int>> input56 = {{4,5},{1,4},{7,8}};
@@ -89,5 +128,9 @@ int main()
     routine::showVectors(merge(input56)) ;
 
     cout << "problem 58 " << lengthOfLastWord("  d   ")<<endl;
+
+
+    cout << "problem 59 " <<endl;
+    routine::showVectors(generateMatrix(7)) ;
     return 0;
 }
