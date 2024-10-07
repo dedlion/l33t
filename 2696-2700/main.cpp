9f16f88a6011d6e014c941c4db64e34e93208716
Your Name <you@example.com>
2024-10-08 00:59:43 +0300

    added problems 2697, 2698

--- 2696-2700/main.cpp
+++ 2696-2700/main.cpp
@@ -36,11 +36,88 @@ int minLength(string s) {
     return s2.size();
 }
 
+//2697. Lexicographically Smallest Palindrome EASY
+string makeSmallestPalindrome(string s) {
+    int len = s.size();
+    for (int i=0; i<len/2; i++)
+    {
+        if (s[i] != s[len-i-1])
+        {
+            if (s[i] < s[len-i-1])
+            {
+                s[len-i-1] = s[i];
+            } else
+            {
+                s[i] = s[len-i-1];
+            }
+        }
+    }
+    return s;
+}
+
+////////
+//2698. Find the Punishment Number of an Integer MEDIUM
+
+//just faster version of stoi for out proble
+int fromString(string num)
+{
+    int result = 0;
+    int base = 1;
+    int len = num.size();
+    for (int i=0; i<len; i++)
+    {
+        int val = num[len-i-1] - '0';   //C style. Get value
+        result += val * base;           //then mult value on 10^n... 123 = 1*10^2+2*10^1+3*10^0
+        base = base*10;                 //change basis
+    }
+    return result;
+}
+
+//check our number i (targetSum) == sum of substrings of i*i (currSum)
+//we use recursion to check all possible variants
+bool isSpecialNumber(int targetSum, string sub, int currSum)
+{
+    if (currSum>targetSum) return false;    //no point to proceed. currSum can only increase
+    int len = sub.size();
+    if (len!=0) //can we recursion deeper?
+    {
+        for (int i=1; i<=len; i++)
+        {
+            int add = fromString(sub.substr(0,i));      //get first i chars
+            string cuttedCopy = sub.substr(i,len-i);    //get all what left
+            bool ok = isSpecialNumber(targetSum, cuttedCopy, currSum+add );   //call self (recursion)
+            if (ok) return ok;
+        }
+    } else
+    {
+        //there is no letters. lets check result
+        if (targetSum==currSum) return true;
+    }
+    return false;
+}
+
+
+int punishmentNumber(int n) {
+    int result =0;
+    //just check all possible values
+    for (int i=9; i<=n; i++)
+    {
+        int square = i*i;
+        if (isSpecialNumber(i,to_string(square),0))
+            result += square;
+    }
+
+
+    return result;
+}
 
 
 int main()
 {
     cout << "Problem 2696 -- " << minLength("CABCDD") <<endl;
 
+    cout << "Problem 2697 -- " << makeSmallestPalindrome("asdasdafg") <<endl;
+
+    cout << "Problem 2698" << punishmentNumber(37) <<endl;
     return 0;
 }
